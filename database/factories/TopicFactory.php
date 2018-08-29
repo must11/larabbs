75ddb18d32a7e178810ab1fc51dff8375d9a6875
must11 <1035462362@qq.com>
2018-08-29 16:43:39 +0000

    Seeding topics and users data

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,17 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sententce=$faker->sentence();
+    $updated_at=$faker->dateTimeThisMonth();
+    $created_at=$faker->dateTimeThisMonth($updated_at);
+
+
     return [
         // 'name' => $faker->name,
+        'title'=>$sententce,
+        'body'=>$faker->text(),
+        'excerpt'=>$sententce,
+        'created_at'=>$created_at,
+        'updated_at'=>$updated_at,
     ];
 });
