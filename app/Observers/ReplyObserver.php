ad8008922befdd3e6cc44adb810c6f6d139df4c0
must11 <your email1035462362@qq.com>
2018-09-09 03:52:47 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -10,8 +11,12 @@ use App\Models\Reply;
 class ReplyObserver
 {
     public function created(Reply $reply)
-    {
+    {   
+        $topic =$reply->topic;
         $reply->topic->increment('reply_count', 1);
+
+        $topic->user->notify(new TopicReplied($reply));
+
     }
 
 
